841d3766e37e005a0511a5d0ca90edce9177a419
Tan Yi Ming <0117133@kdu-online.com>
2018-02-02 22:33:03 +0800

    2D Plain, 2 Characters, Movement for both players

---
+++ Testing Networking/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+}
+
+public class Movement : MonoBehaviour 
+{
+	public float speed;
+	public Boundary boundary;
+	float moveHorizontal, moveVertical;
+
+	void FixedUpdate () 
+	{
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+		if (moveVertical == 0)
+		{
+			moveHorizontal = Input.GetAxis ("Horizontal");
+		}
+
+		if (moveHorizontal == 0)
+		{
+			moveVertical = Input.GetAxis ("Vertical");
+		}
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb.velocity = movement * speed;
+			
+		rb.position = new Vector2 
+		(
+				Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+				Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax)
+		);
+	}
+}
